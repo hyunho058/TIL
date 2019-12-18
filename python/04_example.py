3744855f1f21f75acf78365e6f03b467ba8d5792
hyunho058 <hyunho058@naver.com>
2019-12-18 15:07:00 +0900

    Add python basic

---
+++ python/04_example.py
@@ -0,0 +1,40 @@
+# print() happy hacking!
+
+msg = "happy hacking!"
+
+print(msg)
+print(msg)
+print(msg)
+print(msg)
+print(msg)
+
+# 점심메뉴
+
+menus1 = "순남"
+menus2 = "짜장"
+menus3 = "20층"
+
+print(menus1)
+print(menus2)
+print(menus3)
+
+# List
+
+menus = ["순남", "짜장", "20층"]
+print(menus)
+print(menus[0])
+
+# 전화번호부 저장하기
+
+phone_nums =["02-4564-****", "02-6548-****"]
+print(phone_nums[0])
+
+# Dictionary
+
+dict_nums = {"순남" : "02-4564-****", "짜장" : "02-6548-****"}
+print(dict_nums["순남"])
+print(dict_nums.get("짜장"))
+
+
+ # Shift + del = 행 삭제
+ # Shift + 방향키 = 행 복사
---
+++ python/05_condition.py
@@ -0,0 +1,11 @@
+dust = 120
+
+print(dust > 150)
+print(dust > 100)
+
+if dust > 150:
+    print("매우 나쁨")
+elif dust > 100:
+    print("나쁨")
+else:
+    print("보통")
